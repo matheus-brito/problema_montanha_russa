51011c084d09e5e0eb7841725a871bfe939e4ec0
Matheus Brito <matheusbrito174@gmail.com>
2021-07-13 21:44:23 -0300

    Ajustando comentários do arquivo de configuração

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ c = 4                   # Número de cadeiras por carro
 tm = 10                 # Duração do passeio (s)
 tpIntervalo = [1, 3]    # Intervalo para obter tempo de chegada de passageiros à fila (s)
 te = 1                  # Tempo de embarque/desembarque
-n = 52                  # Número de passageiros
-m = 3                   # Número de carros
+n = 148                 # Número de passageiros (52, 92, 148)
+m = 3                   # Número de carros (1, 2, 3)
